d58af43efdb06ba29b10427e19b0216bff4c539e
DharviPatel2805 <dharvi.pict19@sot.pdpu.ac.in>
2023-10-12 10:48:47 +0530

    location

---
+++ models/Location/City.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const {Schema , model , Types} = require("mongoose");
+const CitySchema = new mongoose.Schema(
+  {
+    CityName: {
+      type: String,
+      required: true,
+    },
+    StateID: {
+      type: Schema.Types.ObjectId,
+      ref:"State",
+      required: true,
+    },
+    // StateName: {
+    //   type: String,
+    //   required: true,
+    // },
+    CountryID: {
+      type: Schema.Types.ObjectId,
+      ref:"Country",
+      required: true,
+    },
+    // CountryName: {
+    //   type: String,
+    //   required: true,
+    // },
+    CityCode: {
+      type: Number,
+      required: true,
+    },
+    LastUpdateBy: {
+      //team id
+      type: String,
+      default: "abcd1234",
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("City", CitySchema);
---
+++ models/Location/State.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const StateSchema = new mongoose.Schema(
+  {
+    StateName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    StateCode: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    CountryName: {
+      type: String,
+      required: true,
+    },
+    CountryID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Country",
+      required: true,
+    },
+    LastUpdateBy: {
+      //team id
+      type: String,
+      // default: "abcd1234",
+      // required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("State", StateSchema);
