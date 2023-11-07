731271515959be7ea0a273e7fd8d733bd4b1fa61
DharviPatel2805 <dharvi.pict19@sot.pdpu.ac.in>
2023-11-07 13:29:19 +0530

    7.11 ziya standee

---
+++ models/Media/MediaPlayList/MediaPlayList.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const MediaPlayListSchema = new mongoose.Schema(
+  {
+    playListName: {
+      type: String,
+      required: true,
+    },
+    // ad_ids_folder: [
+    //   {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "ManageMedia",
+    //     required: true,
+    //   },
+    // ],
+    ad_ids: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "ManageMedia",
+        required: true,
+      },
+    ],
+    IsActive: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("MediaPlayList", MediaPlayListSchema);
