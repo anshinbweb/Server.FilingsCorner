2ae88a0ae49b31bb99cbe99883fd7b2b89d7a266
DharviPatel2805 <dharvi.pict19@sot.pdpu.ac.in>
2023-12-29 13:21:51 +0530

    remove console

--- controllers/Applications/TryJewel.js
+++ controllers/Applications/TryJewel.js
@@ -3,10 +3,8 @@ const TryJewel = require("../../models/Applications/TryJewel");
 exports.getTryJewel = async (req, res) => {
   try {
     const find = await TryJewel.findOne({ _id: req.params._id }).exec();
-    console.log("get Task List", find);
     res.json(find);
   } catch (error) {
-    console.log("log error from get TryJewel", error);
     return res.status(400).send("create dynamic content failed from TryJewel");
   }
 };
@@ -25,7 +23,6 @@ exports.getTryJewelByProduct = async (req, res) => {
 
 exports.createTryJewel = async (req, res) => {
   try {
-    console.log(req.body);
     let productImage = req.file
       ? `uploads/TryOnProducts/${req.file.filename}`
       : null;
@@ -43,7 +40,7 @@ exports.createTryJewel = async (req, res) => {
     }).save();
     res.json(add);
   } catch (err) {
-    console.log("log error from create TryJewel", err);
+    // console.log("log error from create TryJewel", err);
     return res.status(400).send("create dynamic content failed from TryJewel");
   }
 };
@@ -53,7 +50,7 @@ exports.listTryJewel = async (req, res) => {
     const list = await TryJewel.find().sort({ createdAt: -1 }).exec();
     res.json(list);
   } catch (error) {
-    console.log("log error from create TryJewel", error);
+    // console.log("log error from create TryJewel", error);
     return res.status(400).send("list dynamic content failed from TryJewel");
   }
 };
@@ -173,15 +170,11 @@ exports.listTryJewelByParams = async (req, res) => {
     const list = await TryJewel.aggregate(query);
 
     if (list && list.length > 0 && list[0].data && list[0].data.length > 0) {
-      console.log(list[0].data);
-      //   res.json(list[0].data);
       res.json(list);
     } else {
       res.json({ message: "No data to display." });
     }
   } catch (error) {
-    console.log("display error", error);
-    // res.send("err in list", error);
     res.status(500).send("Error in fetching data.");
   }
 };
@@ -191,10 +184,8 @@ exports.removeTryJewel = async (req, res) => {
     const del = await TryJewel.findOneAndRemove({
       _id: req.params._id,
     });
-    console.log(del);
     res.json(del);
   } catch (err) {
-    console.log(err);
     res.status(400).send("delete TryJewel failed");
   }
 };
@@ -217,10 +208,8 @@ exports.updateTryJewel = async (req, res) => {
     fieldvalues,
       { new: true }
     );
-    console.log("edit try jewel", update);
     res.json(update);
   } catch (err) {
-    console.log(err);
     res.status(400).send("update try jewel failed");
   }
 };
